8fd3b5f33e1dc709a5d3987d894b0a9c2e8e1c43
dwh <daiweihong@ireadercity.com>
2020-08-12 10:27:02 +0800

    初次提交

---
+++ animate.js
@@ -0,0 +1,13 @@
+const animateCSS = (element, animation, prefix = 'animate__') =>
+	new Promise((resolve, reject) => {
+		const animationName = `${prefix}${animation}`;
+		const node = document.querySelector(element);
+		node.classList.add(`${prefix}animated`, animationName);
+		function handleAnimationEnd() {
+			node.classList.remove(`${prefix}animated`, animationName);
+			node.removeEventListener('animationend', handleAnimationEnd);
+			resolve('Animation ended');
+		}
+		node.addEventListener('animationend', handleAnimationEnd);
+	});
+export default animateCSS
